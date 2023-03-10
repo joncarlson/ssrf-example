a186a78b95420ec81a674d93243dfa19afde416d
Ben Forshey <benjamin.a.forshey@nasa.gov>
2023-03-10 08:13:35 -0500

    Use `startswith`
    `in` will just check that one string contains another; we want to make sure the string starts with the correct scheme and domain name.

--- lib/allow_list.py
+++ lib/allow_list.py
@@ -1,9 +1,9 @@
-def is_allowed_url(test):
+def is_allowed_url(test=''):
     allow_list = ['https://cmr.earthdata.nasa.gov/',
                   'https://cmr.uat.earthdata.nasa.gov/']
     allowed = []
 
     for url in allow_list:
-        allowed.append(url in test)
+        allowed.append(test.startswith(url))
 
     return any(allowed)
